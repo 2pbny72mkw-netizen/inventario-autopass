96eace3b0617d4c926dac7e9af4f5c187343a25e
2pbny72mkw-netizen <2pbny72mkw@privaterelay.appleid.com>
2026-08-11 08:19:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,11 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(30), nullable=False)
     active = db.Column(db.Boolean, nullable=False, default=True)
-
+    
+    user_code = db.Column(db.String(30), unique=True, index=True)
+    email = db.Column(db.String(180), unique=True, index=True)
+    phone = db.Column(db.String(30), unique=True, index=True)
+    photo_url = db.Column(db.String(500))
 
 class Location(db.Model):
     __tablename__ = "locations"
